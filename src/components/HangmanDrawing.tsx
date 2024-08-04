eb0eaa6d7c7d6f0942d0216438bc956031094c9a
Saif <droconos99@gmail.com>
2024-08-04 16:36:15 +0600

    Add: Finsihed hangman drawing.

--- src/components/HangmanDrawing.tsx
+++ src/components/HangmanDrawing.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+const Head = (
+   <div className="w-12 h-12 rounded-full border-8 border-black absolute top-12 -right-5" />
+);
+const Body = <div className="w-2 h-24 absolute bg-black top-24 right-0" />;
+
+const RightArm = (
+   <div
+      className="w-24 h-2 bg-black absolute top-32 -right-24"
+      style={{ transformOrigin: "left bottom", rotate: "-30deg" }}
+   />
+);
+
+const LeftArm = (
+   <div
+      className="w-24 h-2 bg-black absolute top-32 right-2"
+      style={{ transformOrigin: "right bottom", rotate: "30deg" }}
+   />
+);
+
+const RightLeg = (
+   <div
+      className="w-24 h-2 bg-black absolute top-44 "
+      style={{ transformOrigin: "left bottom", rotate: "60deg", right: "-92px" }}
+   />
+);
+
+const LeftLeg = (
+   <div
+      className="w-24 h-2 bg-black absolute top-44 right-0"
+      style={{ transformOrigin: "right bottom", rotate: "-60deg" }}
+   />
+);
 
 function HangmanDrawing() {
-   return <div>HangmanDrawing</div>;
+   return (
+      <div className="relative">
+         {Head}
+         {Body}
+         {RightArm}
+         {LeftArm}
+         {RightLeg}
+         {LeftLeg}
+         <div className="h-12 w-2 bg-black absolute top-0 right-0" />
+         <div className="h-2 w-48 bg-black ml-32" />
+         <div className="h-96 w-2 bg-black ml-32" />
+         <div className="h-2 w-64 bg-black" />
+      </div>
+   );
 }
 
 export default HangmanDrawing;
